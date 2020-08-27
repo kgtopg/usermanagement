41f7bc0095c37ec686c78694cab2100dd3f91a61
siva0103 <siva.depl.python@gmail.com>
2020-08-27 09:41:54 +0530

    added django restframe work

--- project1/app1/models.py
+++ project1/app1/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class details(models.Model):
-    user_id = models.IntegerField(max_length=50,primary_key=True)
-    user_role_id = models.IntegerField(max_length=50, primary_key=True)
+class users(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    user_role_id = models.IntegerField()
     user_full_name = models.CharField(max_length=20)
     user_email = models.EmailField(max_length=20)
     user_password = models.CharField(max_length=20)
@@ -14,9 +14,3 @@ class details(models.Model):
     user_status = models.CharField(max_length=100)
     created_date = models.DateField()
     timestamp = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['created']
-
-    def __str__(self):
-        return self.title
